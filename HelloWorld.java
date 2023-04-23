373e1d01fa5527e50a7e6dc7cca3a09d4cb15b22
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-23 18:43:33 +0530

    helloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//Java is a verbose programming language so there is some boilerplate code that we add with every program.
+public class HelloWorld{
+
+public static void main(String[] args){
+//its called the main func and is the emtry point of the program
+
+System.out.println("Hello World it's me Vaishnavi and I love noodles.");
+
+}
+}
