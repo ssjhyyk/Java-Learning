8ede318d82bd8ed49e8596a8b6e19461d6b1e628
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-23 23:27:19 +0530

    control statements

--- src/fortuneTeller/fortuneTeller.java
+++ src/fortuneTeller/fortuneTeller.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class fortuneTeller{
-    public static void main(string[] args){
+    public static void main(String[] args){
         System.out.println("Pick a number between 1 and 10");
         Scanner scanner = new Scanner(System.in);
         int inputNum = scanner.nextInt();
 
-        
+        if(inputNum < 5){
+            System.out.println("Enjoy the good luck a friend brings you");
+        }
+        else{
+            System.out.println("Your shoe selection will make you very happy today");
+        }
     }
 }
