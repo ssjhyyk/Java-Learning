32f6a9011c80f0f4df058dd7be4a2fa905295bec
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-23 19:43:27 +0530

    main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    //The main function is the first function called by the runtime system and ultimately your code must be connected to this call in order to be executed by the program.
+    public static void main(String[]  args){
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'V';
+        char studentLastInitial = 'S';
+        boolean hasPerfectAttendace = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+    }
+}
