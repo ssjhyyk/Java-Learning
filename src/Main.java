7347841d1d98391756bc3b21f814e2c22c7ad77b
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-23 23:22:11 +0530

    edit 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,30 @@
+import java.util.Scanner;
+
 public class Main{
     //The main function is the first function called by the runtime system and ultimately your code must be connected to this call in order to be executed by the program.
     public static void main(String[]  args){
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'V';
-        char studentLastInitial = 'S';
+    
         boolean hasPerfectAttendace = true;
+        String thisIsString = "Temporary";
+
+        char studentFirstInitial = thisIsString.charAt(0);
+        char studentLastInitial = thisIsString.charAt(0);        
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
+        System.out.println(thisIsString);
+
+        System.out.println(studentFirstInitial + " " + studentLastInitial + " has a GPA of " + studentGPA);
+
+
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        System.out.println(studentGPA);
+
     }
 }
+
---
+++ src/fortuneTeller/fortuneTeller.java
@@ -0,0 +1,9 @@
+public class fortuneTeller{
+    public static void main(string[] args){
+        System.out.println("Pick a number between 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+        int inputNum = scanner.nextInt();
+
+        
+    }
+}
