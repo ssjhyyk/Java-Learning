e3de3678b2c6c7bd389825b213be3cd77052736d
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-24 22:04:28 +0530

    spotify

--- src/Main.java
+++ src/Main.java
@@ -3,27 +3,41 @@ import java.util.Scanner;
 public class Main{
     //The main function is the first function called by the runtime system and ultimately your code must be connected to this call in order to be executed by the program.
     public static void main(String[]  args){
-        int studentAge = 15;
-        double studentGPA = 3.45;
+        // int studentAge = 15;
+        // double studentGPA = 3.45;
     
-        boolean hasPerfectAttendace = true;
-        String thisIsString = "Temporary";
+        // boolean hasPerfectAttendace = true;
+        // String thisIsString = "Temporary";
 
-        char studentFirstInitial = thisIsString.charAt(0);
-        char studentLastInitial = thisIsString.charAt(0);        
+        // char studentFirstInitial = thisIsString.charAt(0);
+        // char studentLastInitial = thisIsString.charAt(0);        
 
-        System.out.println(studentAge);
-        System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
-        System.out.println(thisIsString);
+        // System.out.println(studentAge);
+        // System.out.println(studentGPA);
+        // System.out.println(studentFirstInitial);
+        // System.out.println(studentLastInitial);
+        // System.out.println(thisIsString);
 
-        System.out.println(studentFirstInitial + " " + studentLastInitial + " has a GPA of " + studentGPA);
+        // System.out.println(studentFirstInitial + " " + studentLastInitial + " has a GPA of " + studentGPA);
 
 
-        Scanner input = new Scanner(System.in);
-        studentGPA = input.nextDouble();
-        System.out.println(studentGPA);
+        // Scanner input = new Scanner(System.in);
+        // studentGPA = input.nextDouble();
+        // System.out.println(studentGPA);
+
+        // Scanner input = new Scanner(System.in);
+        // boolean isOnRepeat = true;
+        // while(isOnRepeat){
+        //     System.out.println("Playing current song");
+        //     System.out.println("Would you like to take this song off of repeat?If so, answer yes");
+        //     String userInput = input.next();
+        //     if(userInput.equals("Yes")){
+        //         isOnRepeat = false;
+        //     }
+        // }
+
+
+        
 
     }
 }
