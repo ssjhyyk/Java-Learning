cc4e0d7307f1b25b7557bb49a74e07db52e38158
ssjhyyk <112963608+ssjhyyk@users.noreply.github.com>
2023-04-28 09:34:25 +0530

    commit 7

---
+++ ap25.java
@@ -0,0 +1,10 @@
+class ap25{
+public static void main(String[] ar){
+int a = 10;
+if(a == 10){
+System.out.println("hi");
+}else{
+System.out.println("Bye");
+}
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -36,25 +36,85 @@ public class Main{
         //     }
         // }
 
-        String question = "water is wet. comment.";
-        String choiceOne = "yes";
-        String choiceTwo = "no";
-        String choiceThree = "good question";
+        // String question = "water is wet. comment.";
+        // String choiceOne = "yes";
+        // String choiceTwo = "no";
+        // String choiceThree = "good question";
 
-        String correctAnswer = choiceThree;
+        // String correctAnswer = choiceThree;
 
-        System.out.println(question + "\n Choices \n" + choiceOne + "\n" + choiceTwo + "\n" + choiceThree);
+        // System.out.println(question + "\n Choices \n" + choiceOne + "\n" + choiceTwo + "\n" + choiceThree);
 
-        Scanner input = new Scanner(System.in);
-        String userAnswer = input.next();
+        // Scanner input = new Scanner(System.in);
+        // String userAnswer = input.next();
 
-        if(userAnswer != correctAnswer.toLowerCase()){
-            System.out.println("Wrong Answer, the correct answer is " + correctAnswer);
-        }
-        else{
-            System.out.println("Right Answer, Good Job.");
-        }
+        // if(userAnswer != correctAnswer.toLowerCase()){
+        //     System.out.println("Wrong Answer, the correct answer is " + correctAnswer);
+        // }
+        // else{
+        //     System.out.println("Right Answer, Good Job.");
+
+        // int a;
+        // // simple assignment
+
+        // int b,c,d;
+        // // chain assignment
+
+        // & - checks both if true or false
+        // in case of && if the first statement is true then it doesnt check the other statement
+        // && is called conditional and 
+        // || is conditional or
+
+//         long x = 8.8; // Line 1
+
+// float y= x; //Line 2
+
+// double z=x+y; //Line 3
+
+// System.out.println(z);
+        
+//         boolean a= true;
+
+// boolean b=!true;
+
+// boolean c= a|b;
+
+// boolean d=a&b;
+
+// boolean e= d?b:c;
+
+// System.out.println(d+""+e);
+
+// boolean x = true;
+
+// boolean y = false;
+
+// System.out.print(!(x && y));
+
+// System.out.print(!(x||y));
+
+// System.out.print(x&&y);
+
+
+//     
+// double num = 2._718;
+
+// short x =13;   // Line 1
+
+// x = x*5;  // Line 2
+
+// System.out.print(x);
+
+// double d1 = 5f;  //Line1
+// double d2 = 5.0; //Line 2 
+// float f2 = 5.0;  //Line 3
+// float f1 = 5f;  //Line 4
+
+// double num = 2._718;
+
+int num = 9.9_29;
 
     }
 }
+
 
